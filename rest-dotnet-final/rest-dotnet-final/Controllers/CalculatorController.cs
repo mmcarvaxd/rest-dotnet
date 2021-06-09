40c2bd764ebacb01bbf35e171af22aa46f79b74b
Matheus Martins de carvalho <matheus.carvalho@mobilemed.com.br>
2021-06-09 15:08:43 -0300

    Create Sum Request

---
+++ rest-dotnet-final/rest-dotnet-final/Controllers/CalculatorController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rest_dotnet_final.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double i;
+
+            return double.TryParse(strNumber, out i);
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            if(IsNumeric(strNumber))
+            {
+                return decimal.Parse(strNumber);
+            }
+
+            return 0;
+        }
+
+    }
+}
