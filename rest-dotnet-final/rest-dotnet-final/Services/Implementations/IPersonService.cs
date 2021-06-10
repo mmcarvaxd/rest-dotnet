b3058371fe78589512da4ac777bc679c46d78b59
Matheus Martins de carvalho <matheus.carvalho@mobilemed.com.br>
2021-06-10 12:30:40 -0300

    Add Person Interface

---
+++ rest-dotnet-final/rest-dotnet-final/Services/Implementations/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using rest_dotnet_final.Model;
+using System.Collections.Generic;
+
+namespace rest_dotnet_final.Services.Implementations
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindByAll(long id);
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
