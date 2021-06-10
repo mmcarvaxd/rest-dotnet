323fa01faecfc3103c862c7575f74e5ab74e3460
Matheus Martins de carvalho <matheus.carvalho@mobilemed.com.br>
2021-06-10 13:14:03 -0300

    Add and Configure Entity to Mysql

---
+++ rest-dotnet-final/rest-dotnet-final/Model/Context/MysqlContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rest_dotnet_final.Model.Context
+{
+    public class MysqlContext : DbContext
+    {
+        public MysqlContext()
+        {
+
+        }
+
+        public MysqlContext(DbContextOptions<MysqlContext> options) : base (options)
+        {
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
