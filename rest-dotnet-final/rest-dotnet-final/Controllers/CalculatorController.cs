3acb446751d62e965665bd1aefa102dfc346fdf3
Matheus Martins de carvalho <matheus.carvalho@mobilemed.com.br>
2021-06-10 12:23:04 -0300

    Finished Calculator

--- rest-dotnet-final/rest-dotnet-final/Controllers/CalculatorController.cs
+++ rest-dotnet-final/rest-dotnet-final/Controllers/CalculatorController.cs
@@ -19,11 +19,44 @@ namespace rest_dotnet_final.Controllers
         }
 
         [HttpGet("sum/{firstNumber}/{secondNumber}")]
-        public IActionResult Get(string firstNumber, string secondNumber)
+        public IActionResult Sum(string firstNumber, string secondNumber)
         {
             if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
             {
                 var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("subtract/{firstNumber}/{secondNumber}")]
+        public IActionResult Subtract(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("multiply/{firstNumber}/{secondNumber}")]
+        public IActionResult Multiply(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("divide/{firstNumber}/{secondNumber}")]
+        public IActionResult Divide(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
                 return Ok(sum.ToString());
             }
             return BadRequest("Invalid Input");
