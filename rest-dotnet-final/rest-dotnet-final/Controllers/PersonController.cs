e138d920f0444720a473288193dfbc73504e6d92
Matheus Martins de carvalho <matheus.carvalho@mobilemed.com.br>
2021-06-10 12:58:26 -0300

    Finish Basic Implementation

--- rest-dotnet-final/rest-dotnet-final/Controllers/PersonController.cs
+++ rest-dotnet-final/rest-dotnet-final/Controllers/PersonController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace rest_dotnet_final.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class PersonController : ControllerBase
     {
         private readonly ILogger<PersonController> _logger;
@@ -53,7 +53,7 @@ namespace rest_dotnet_final.Controllers
             return Ok(_personService.Update(person));
         }
 
-        [HttpGet("{id}")]
+        [HttpDelete("{id}")]
         public IActionResult Delete(long id)
         {
             _personService.Delete(id);
